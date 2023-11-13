992df0eeca55be3d17079691b4293a1f7aeffe18
bunny983 <128358297+bunny983@users.noreply.github.com>
2023-11-13 23:33:14 +0530

    Update swserver.cs

--- swserver.c
+++ swserver.c
@@ -26,3 +26,50 @@ len=sizeof(&caddr);
 lfd=accept(sfd,(struct sockaddr*)&caddr,&len);
 printf(" Enter the text : \n");
 scanf("%s",str);
+i=0;
+while(i<strlen(str))
+{
+memset(frame,0,20);
+strncpy(frame,str+i,SIZE);
+printf(" Transmitting Frames. ");
+len=strlen(frame);
+for(j=0;j<len;j++)
+{
+printf("%d",i+j);
+sprintf(temp,"%d",i+j);
+strcat(frame,temp);
+}
+printf("\n");
+write(lfd,frame,sizeof(frame));
+read(lfd,ack,20);
+sscanf(ack,"%d",&status);
+if(status==-1)printf(" Transmission is successful. \n");
+else
+{
+printf(" Received error in %d \n\n",status);
+printf("\n\n Retransmitting Frame. ");
+for(j=0;;)
+{
+frame[j]=str[j+status];
+printf("%d",j+status);
+j++;
+if((j+status)%4==0)
+break;
+}
+printf("\n");
+frame[j]='\0';
+len=strlen(frame);
+for(j=0;j<len;j++)
+{
+sprintf(temp,"%d",j+status);
+strcat(frame,temp);
+}
+write(lfd,frame,sizeof(frame));
+}
+i+=SIZE;
+}write(lfd,"exit",sizeof("exit"));
+printf("Exiting\n");
+sleep(2);
+close(lfd);
+close(sfd);
+}
